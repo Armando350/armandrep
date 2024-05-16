bbdef97faaf3fabdb3aed665281563667bda4156
Armando350 <[armandinho_sume@hotmail.com]>
2024-05-16 19:53:41 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+def par(numero):
+    if (numero%2) == 0:
+        print("Este numero é par")
+    else:
+        print("Este numero é impar")
+
+if __name__ == "__main__":
+    par(int(input("Digite um numero: ")))
+
+    
